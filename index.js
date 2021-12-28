8918c798c7a512a3bff8a2e487335eb36f09c1c3
Sanyas Dada <sanyasdada@gmail.com>
2021-12-28 14:31:25 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// console.log("hello,Sanyas dada, Software enginnering");
+// let name = "Dhankumari"; // String literal
+// console.log(name);
+// let number = 10;
+// let num = 0.99;
+// let isTrue = false;
+// let sanyasdad = null;
+// let knowledge = undefined;
+let person ={
+    name:"Dhankumari",
+    dream:"Becoming a full stack developer",
+    age:23
+}
+console.log(person);
+// Dot Notation
+//person.name = "Poshraj subedi";
+//console.log(person.name);
+//Brackeyt notation 
+person['name'] = "Laxmi poudel";
+
+console.log(person.name);
+let selectedColor = ['red','yellow'];
+//console.log(selectedColor[0]);
+selectedColor[3] = 'Black';
+selectedColor[4] = 10;
+
+console.log(selectedColor.push("true"));
+console.log(selectedColor.pop());
+
+
+
+
+
+
+
+
